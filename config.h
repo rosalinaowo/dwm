e25652b9709a0a417e45187813b06a62899b5656
krashy-i386 <riccardoromagnoli06@gmail.com>
2022-06-26 14:59:47 +0200

    custom bar script + other things

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Regular:size=11:autohint=true", "Noto Color Emoji:style=Regular:size=10:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Regular:size=10:autohint=true", "Noto Color Emoji:style=Regular:size=10:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 /* default colorscheme */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -89,25 +89,33 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", white, "-sb", magenta, "-sf", white, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
+static const char *lock[] = { "/home/rikka/scripts/lock.sh", NULL };
+static const char *rofi[] = { "rofi", "-show", "drun", NULL };
+static const char *powermenu[] = { "rofi", "-show", "power-menu", "-modi", "power-menu:~/.config/rofi/bin/rofi-power-menu"};
+static const char *shot[] = { "flameshot", "gui", NULL };
+static const char *web[] = { "firefox", NULL };
+
+/* Media binds */
 static const char *upvol[]   = { "pactl", "set-sink-volume", "1", "+5%",     NULL };
 static const char *downvol[] = { "pactl", "set-sink-volume", "1", "-5%",     NULL };
 static const char *mutevol[] = { "pactl", "set-sink-mute",   "1", "toggle",  NULL };
 static const char *playpause[] = { "playerctl", "play-pause", NULL };
 static const char *next[] = { "playerctl", "next", NULL };
 static const char *prev[] = { "playerctl", "previous", NULL };
-static const char *lock[] = { "betterlockscreen", "-l", "dimblur", "--display", "1", NULL };
-static const char *rofi[] = { "rofi", "-show", "drun", NULL };
-static const char *powermenu[] = { "rofi", "-show", "power-menu", "-modi", "power-menu:~/.config/rofi/bin/rofi-power-menu"};
+
 
 #include "X11/XF86keysym.h"
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_c,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,												XK_r,			 spawn,					 {.v = rofi} },
+	{ MODKEY,												XK_r,			 spawn,					 {.v = rofi } },
 	{ MODKEY,					              XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,												XK_Escape, spawn,					 {.v = powermenu } },
-	{ 0,														XK_Print,	 spawn,					 SHCMD("flameshot gui") },
+	{ MODKEY|ShiftMask,							XK_l,			 spawn,					 {.v = lock } },
+	{ Mod1Mask,											XK_space,	 spawn,					 SHCMD("switcher") },
+	{ 0,														XK_Print,	 spawn,					 {.v = shot } },
+	{ MODKEY,												XK_w,			 spawn,					 {.v = web } },
 	{ 0,														XF86XK_AudioLowerVolume,	 spawn,	{.v = downvol} },
 	{ 0,														XF86XK_AudioRaiseVolume,	 spawn,	{.v = upvol} },
 	{ 0,														XF86XK_AudioMute,					 spawn,	{.v = mutevol} },
--- scripts/status.sh
+++ scripts/status.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
 
 # colors
-. themes/paradise
+. ~/src/dwm/scripts/themes/paradise
 
-sep=" | "
+sep=$(printf "^c#bbbbbb^|^d^")
 
 cpu() {
-	usage=$(top -bn1 | grep Cpu | awk '{print $2}')\%%
-	printf "^c$blue^ ^d^ ^c$magenta^$usage^d^"
+	usage=$(top -bn1 | grep Cpu | awk '{print $2}')
+	printf "^c$blue^ ^d^^c$magenta^$usage%%^d^"
 }
 
 mem() {
 	used=$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)
-	printf "^c$blue^ ^d^ ^c$magenta^$used^d^"
+	printf "^c$blue^ ^d^^c$magenta^$used^d^"
 }
 
 vol() {
 	vol=$(pamixer --get-volume-human | sed s/%//)
-	printf "^c$blue^ ^d^ ^c$magenta^$vol%%^d^"
+	if [ "$vol" = "muted" ]; then
+		printf "^c$blue^ ^d^^c$magenta^$vol^d^"
+	else
+		if [ "$vol" -gt 0 ] && [ "$vol" -le 33 ];then
+			printf "^c$blue^ ^d^^c$magenta^$vol%%^d^"
+		elif [ "$vol" -gt 33 ] && [ "$vol" -le 66 ];then
+			printf "^c$blue^ ^d^^c$magenta^$vol%%^d^"
+		else
+			printf "^c$blue^ ^d^^c$magenta^$vol%%^d^"
+		fi
+	fi
 }
 
 datetime() {
 	date=$(date "+%a %d/%m/%y")
 	time=$(date "+%H:%M")
-	printf "^c$blue^ ^d^ ^c$magenta^$date^d^ "
-	printf "^c$blue^ ^d^ ^c$magenta^$time^d^"
+	printf "^c$blue^ ^d^^c$magenta^$date^d^ $sep "
+	printf "^c$blue^ ^d^^c$magenta^$time^d^"
 }
 
 while true; do
-	sleep 1 && xsetroot -name "$(mem) $(cpu) $(vol) $(datetime)"
+	sleep 1 && xsetroot -name "$(mem) $sep $(cpu) $sep $(vol) $sep $(datetime)"
 done
